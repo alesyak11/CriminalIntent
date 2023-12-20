f4cf20c9ed772128b6f2096ed2623053143ff807
Dmitriy <dmitriybogatyrev2001@gmail.com>
2023-12-20 13:11:38 +0700

    yt,jkmijt ghtj,hf;tyt

--- app/src/main/java/com/sample/criminalintent/Crime.kt
+++ app/src/main/java/com/sample/criminalintent/Crime.kt
@@ -4,9 +4,10 @@ import java.util.Date
 import java.util.UUID
 
 class Crime {
-    data class CrimeDetails(val id: UUID = UUID.randomUUID(),
-                     var title: String = "",
-                     var date: Date = Date(),
-                     var isSolved: Boolean = false)
-
+    data class CrimeDetails(
+        val id: UUID = UUID.randomUUID(),
+        var title: String = "",
+        var date: Date = Date(),
+        var isSolved: Boolean = false
+    )
 }
