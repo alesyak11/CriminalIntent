f367c6db54d98ac5286724cc101c9e0c27b7d075
Dmitriy <dmitriybogatyrev2001@gmail.com>
2023-12-20 17:31:36 +0700

    Наблюдение за изменениями

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -3,6 +3,7 @@ package com.sample.criminalintent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import java.util.UUID
 
 private const val TAG = "CrimeFragment"
@@ -22,10 +25,18 @@ class CrimeFragment : Fragment(){
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+    private val crimeDetailViewModel:
+            CrimeDetailViewModel by lazy {
+                ViewModelProviders.of(this).get(CrimeDetailViewModel::class.java)
+            }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId: UUID =
+            arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        crimeDetailViewModel.loadCrime(crimeId)
+// Загрузка преступления из базы данных
     }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,6 +51,20 @@ class CrimeFragment : Fragment(){
         }
 
         return view
+    }
+    override fun onViewCreated(view: View,
+                               savedInstanceState: Bundle?) {
+        super.onViewCreated(view,
+            savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            }
+        )
     }
     override fun onStart() {
         super.onStart()
@@ -74,6 +99,16 @@ class CrimeFragment : Fragment(){
             }
         }
     }
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
+
+    }
+
     companion object {
         fun newInstance(crimeId: UUID): CrimeFragment {
             val args = Bundle().apply {
